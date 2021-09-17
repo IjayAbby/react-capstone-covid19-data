2e4ac44d548dd94093b694adcb713ccff3a7edfe
Ijay Abby <inyangasi4@gmail.com>
2021-09-17 20:18:21 +0300

    Update routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import './App.css';
+import React, { useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import getCovid from './redux/slices/covidSlice';
+import Home from './components/Home';
+import Details from './components/Details';
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(async () => {
+    await dispatch(getCovid());
+  }, []);
+
+  const countries = useSelector((state) => state.covidReducer.covidCountries);
+
+  const routes = countries.map((country) => (
+    <Route key={country.name[0]} path={`/${country.name[0].toLowerCase()}`}>
+      <Details name={country.name[0]} />
+    </Route>
+  ));
+
   return (
-    <div className="App">
-      <h1>Hello there</h1>
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          { routes }
+        </Switch>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
